6560f4e815c277d9c0d1fca2fa0c189a62a271be
walidmujahid <walid.mujahid.dev@gmail.com>
2017-12-22 19:06:58 -0500

    update

--- giveawayhelper/helper.py
+++ giveawayhelper/helper.py
@@ -4,7 +4,9 @@ from random import sample
 
 def tokenise_students(students: list):
     """Returns a dict where a unique random number is assigned to a student."""
-    return dict(zip(sample(range(1, len(students)+1), len(students)), students))
+    population_of_class = sample(range(1, len(students)+1), len(students))
+
+    return dict(zip(population_of_class, students))
 
 
 if __name__ == '__main__':
